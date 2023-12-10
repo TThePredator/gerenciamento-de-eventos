8a9e3d548da43cb0f87cfa656fd5b88063ea24ce
Pedro <pedroigor.projetos@gmail.com>
2023-12-10 16:02:49 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import EventsPage from './pages/EventsPage';
+import EventCreatePage from './pages/EventCreatePage'; 
+import EventDetailsPage from './pages/EventDetailsPage';
+
+function App() { 
+  return ( 
+    <Router> 
+      <Routes> 
+        <Route path="/" element={<EventsPage />} /> 
+        <Route path="/create-event" element={<EventCreatePage />} /> 
+        <Route path="/events/:id" element={<EventDetailsPage />} /> 
+      </Routes> 
+    </Router> 
+  );
+} 
+ 
+export default App;
